dab7bdfb6c03d98868c6e0c05e3d60a6a2d561e3
Limehouse <limehouse@bitsign.uk>
2025-08-06 16:26:10 +0100

    Add icons and new content

--- js/pw.js
+++ js/pw.js
@@ -1,31 +1,72 @@
 const me = {
   contact : {
-    fname : "Phillip",
-    sname : "Webb",
-    phone : "01480 878135",
-    email : "phillip@londonbusred.co.uk"
+    firstname : "Phillip",
+    surname : "Webb",
+    phone : "07719 123327",
+    email : "phillip.webb@nntp.uk",
+    url : "https://www.linkedin.com/in/iamphillipwebb",
+    about : [
+      "A self-taught developer who started with HTML, PHP and MySQL.  Currently exploring Vue.JS, I am keen to learn new languages that I can use in personal and professional projects, with previous experience using Golang, Java and AutoIt3.",
+      
+    ],
+    skills : [
+      "HTML",
+      "CSS",
+      "PHP",
+      "JavaScript",
+      "Java",
+      "Golang",
+      "AutoIt3",
+      "TypeScript",
+      "Oracle",
+      "MySQL",
+      "SQLite",
+      "Microsoft SQL",
+      "DP4",
+      "EPOS",
+      "Pharmacovigilance",
+      "Angular",
+      "Vue.JS",
+      "Spring Boot"
+    ]
   },
   employment : [
     {
-      employer : "Assured Information Systems Limited",
-      role : "Software Support Analyst",
-      from : "2013-11",
-      to : "Present",
-      description : "In this role I provide support to users of our human and veterinary pharmacovigilance software from a global customer base served by domestic and foreign public health agencies and the pharmaceutical and life sciences industries.",
+      employer : "Ennov, St. Neots",
+      role : "Development Engineer",
+      from : "2021-11",
+      to : "2025-09",
+      description : "I am part of a scrum team designing, developing and maintaining a pharmacovigilance web application, using Angular, Java and Oracle database.",
+      skills : ["Oracle", "HTML", "CSS", "JavaScript", "TypeScript", "Angular", "Jenkins", "Solr", "Linux", "SQL", "REST APIs", "Java", "Liquibase", "IntelliJ IDEA", "Scrum", "Git", "Microservices", "Golang", "DevExtreme", "Spring Data JPA"],
       bullets : [
-        "Prioritising and managing support requests until resolved",
-        "Troubleshooting Microsoft RDS RemoteApp problems",
-        "Querying and updating Oracle Database to resolve customer issues",
-        "Preparing and distributing monthly issues reports to customers",
-        "I have developed several small applications used by my co-workers"
+        "Developing new API endoints for RESTful microservices using Spring Boot",
+        "Communicating progress, and discussion of potential issues in daily standup meetings",
+        "Creating and maintaining Angular front-end functionality",
+        "Developed and maintained deployment pipelines using Jenkins",
       ]
     },
     {
-      employer : "Tensor plc",
+      employer : "Assured Information Systems, St. Neots",
+      role : "Software Support Analyst",
+      from : "2013-11",
+      to : "2021-11",
+      description : "Experienced in managing and supporting a pharmacovigilance database, troubleshooting software issues and providing user support, and ensuring data integrity withing the database.",
+      skills : [],
+      bullets : [
+        "Provided technical support and training for users, addressing support tickets promptly",
+        "Implemented system and database enhancements to improve functionality",
+        "Produced monthly issue reports for customers",
+        "Designed, developed and maintained C# .NET utilities for internal use by the support team",
+        "Developed a web interface to help customers troubleshoot connectivity issues with our services"
+      ]
+    },
+    {
+      employer : "Tensor, St. Neots",
       role : "Helpdesk Analyst",
-      from : "2010-02",
+      from : "2010-07",
       to : "2013-10",
       description : "Application and hardware support for customers using Tensor time and attendance, access control, personnel and energy management systems. Security vetted as non-police personnel by West Midlands Police.",
+      skills : [],
       bullets : [
         "Microsoft SQL and MSDE database configuration and support",
         "Hardware installation advice given to electrical engineers",
@@ -34,11 +75,12 @@ const me = {
       ]
     },
     {
-      employer : "NHS Anglia Support Partnership",
+      employer : "NHS Anglia Support Partnership, Huntingdon",
       role : "Service Desk Analyst",
       from : "2010-02",
       to : "2010-07",
       description : "First line service desk role delivering IT and Facilities support to NHS Strategic Health Authority and Primary Care Trust staff across the Anglia region.",
+      skills : [],
       bullets : [
         "Microsoft Windows Active Directory account administration",
         "Microsoft Exchange 2007 account management via OWA",
@@ -47,29 +89,59 @@ const me = {
       ]
     },
     {
-      employer : "Itim Limited",
+      employer : "Itim, Huntingdon",
       role : "Software Support Analyst",
       from : "2006-12",
       to : "2010-02",
-      description : "First and second line support role, including out-of-hours support for retailers in the UK and Europe using Itim end-to-end retail applications.",
+      description : "Second line support role, including out-of-hours support for retailers in the UK and Europe using Itim end-to-end retail applications.",
+      skills : [],
       bullets : [
-        "EFT and EPoS software and hardware support for floor and back office staff",
+        "EFT and EPoS software and hardware support for shop floor and back office staff",
         "Responsible for EPoS provision and management for new and refitted stores",
         "Incident resolution using remote access applications",
         "Microsoft SQL and DP4 database management"
       ]
     },
     {
-      employer : "iCare UK Limited",
+      employer : "iCare UK, St. Neots",
       role : "Support Technician",
       from : "2005-05",
       to : "2006-11",
-      description : "IT support role for UK and European customers in a small office or home environment. In addition to this role I provided administrative support to Sales staff.",
+      description : "IT support role for UK and European customers in small or home office environments. In addition to this role I provided administrative support to Sales staff.",
+      skills : [],
       bullets : [
-        "Microsoft Windows and Office application support",
-        "Maintaining accurate records of customer requests, incidents and assets",
-        "Producing quotes, invoices and contracts for scheduled work",
-        "Giving technical sales advice to customers"
+        "Provided Microsoft Windows and Office application support to customers",
+        "Maintained records of customer requests, incidents and assets",
+        "Produced quotes, invoices and contracts for scheduled work",
+        "Gave technical sales advice to customers"
+      ]
+    },
+    {
+      employer : "Hotspot Bakery, St. Neots",
+      role : "Baker",
+      from : "2004-06",
+      to : "2006-11",
+      description : "A skilled baker with experience producing frozen pies, pastries, bread and cakes while maintaining high standards of quality and safety.",
+      skills : [],
+      bullets : [
+        "Operated bakery equipment and maintained a clean workspace to comply with regulations",
+        "Managed inventory of ingredients and supplies to meet production demands",
+        "Ensured quality and standards to maintain product consistency",
+        "Contributed with team members to increase efficiency of the production process"
+      ]
+    },
+    {
+      employer : "T-Auctions",
+      role : "Hardware Technician",
+      from : "2003-07",
+      to : "2004-06",
+      description : "IT recycling technician with hands-on experience in managing the lifecycle of decimmissioned electronic equipment.",
+      skills : [],
+      bullets : [
+        "Effienciently processed pallets of printers, monitors, desktop and notebook computers",
+        "Destroyed data to ensure compliance with data protection regulations",
+        "Cleaned and refurbished devices to meet quality standards for resale",
+        "Imaged devices and prepared them for onsite, and online auction sales"
       ]
     }
   ],
@@ -82,11 +154,7 @@ const me = {
       display : "list",
       courses : [
         {
-          name : "My Digital Life (TU100)",
-          level : ""
-        },
-        {
-          name : "Technologies in Practice (TM129)",
+          name : "Web Technologies (TT284)",
           level : ""
         },
         {
@@ -94,7 +162,11 @@ const me = {
           level : ""
         },
         {
-          name : "Object-oriented Java Programming (M250)",
+          name : "Technologies in Practice (TM129)",
+          level : ""
+        },
+        {
+          name : "My Digital Life (TU100)",
           level : ""
         }
       ]
@@ -156,7 +228,8 @@ const me = {
   ],
   interests : [
     "For the past 17 years I have regularly played inline hockey with a local skating club. As a member of the club committee I am responsible for providing email and web hosting services and maintenance of email distribution lists. Players have access to an online RSVP system, which I developed using PHP and MySQL. I am also responsible for the maintenance of this system.",
-    "I have a broad interest in IT related fields and take particular interest in IT security and Linux related news."
+    "I have a broad interest in IT related fields and take particular interest in IT security and Linux related news.",
+    "I am a full-time desktop Linux user, and manage several self-hosted services using LXC containers"
   ],
   getContact : function()
   {
@@ -178,6 +251,7 @@ const me = {
 
 function addItem(id, val)
 {
+  if (id === "url") val = `<a href="${val}" target="_blank">${val}</a>`;
   document.getElementById(id).insertAdjacentHTML("beforeend", val);
 }
 
@@ -219,36 +293,38 @@ function getDuration(dateFrom, dateTo)
 {
   let from = new Date(dateFrom); // create date object
   let to = (dateTo == "Present") ? new Date() : new Date(dateTo); // use today's date if to is 'Present'
-  
+
   let monthsCalc = to.getMonth() - from.getMonth(); // calculate months between
   let yearsCalc = (12 * (to.getFullYear() - from.getFullYear()));
   let monthsTotal = monthsCalc + yearsCalc;
-  
+
   return { "years" : Math.floor(monthsTotal / 12), "months" : monthsTotal%12 };
 }
 
 /**
  * Build the page
  */
-addItem("name", `${me.getContact().fname} ${me.getContact().sname}`);
-addItem("phone", `phone: ${me.getContact().phone},`);
-addItem("email", `email: ${me.getContact().email}`);
+addItem("name", `${me.getContact().firstname} ${me.getContact().surname}`);
+addItem("phone", `${me.getContact().phone}`);
+addItem("email", `${me.getContact().email}`);
+addItem("url", `${me.getContact().url}`);
 
 me.getEmployers().forEach(j => addItem("employment", `
   <div class="job">
-    <h4>${j.role}</h4>
-    <div>${j.employer}<span class="fromto">${getPeriod(j.from)} &ndash; ${getPeriod(j.to)}</span></div>
-    <div>${j.description}</div>
+    <h4><i class="fa-solid fa-user"></i>${j.role}</h4>
+    <div class="employer"><i class="fa-regular fa-building"></i>${j.employer}<span class="fromto"><i class="fa-regular fa-calendar"></i>${getPeriod(j.from)} &ndash; ${getPeriod(j.to)}</span></div>
+    <div class="description">${j.description}</div>
     <ul>
-      <li>${j.bullets.join("</li><li>")}</li>
-    </li>
+      ${j.bullets.length > 0 ? `<li>${j.bullets.join("</li><li>")}</li>` : '' }
+    </ul>
+    <div style="font-size: smaller; font-style: italic;" class="skills">${j.skills.sort().join(", ")}</div>
   </div>
 `));
 
 me.getEducators().forEach(e => addItem("education", `
   <div class="school">
-    <h4>${e.institute}</h4>
-    <div>${e.location}<span class="fromto">${getPeriod(e.from)} &ndash; ${getPeriod(e.to)}</span></div>
+    <h4><i class="fa-solid fa-building-columns"></i>${e.institute}</h4>
+    <div class="location"><i class="fa-solid fa-location-dot"></i>${e.location}<span class="fromto"><i class="fa-regular fa-calendar"></i>${getPeriod(e.from)} &ndash; ${getPeriod(e.to)}</span></div>
     <ul>
       <li>${e.courses.map(c => c.name + (c.level.length > 0 ? `, <span class="level">${c.level}</span>` : "")).join("</li><li>")}</li>
     </ul>
@@ -258,3 +334,9 @@ me.getEducators().forEach(e => addItem("education", `
 me.getInterests().forEach(i => addItem("interests", `
   <p>${i}</p>
 `));
+
+me.getContact().about.forEach(i => addItem("contact", `
+  <p>${i}</p>
+  `));
+
+// addItem("contact", me.getContact().skills.sort().join(", "))
